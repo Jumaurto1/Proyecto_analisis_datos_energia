a8efb3e3cd70d25da6a0257a2bad67a7134e812d
Jumaurto <jumaurto1@gmail.com>
2025-09-28 11:43:05 -0500

    Modificación de start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Levanta el notebook con Voila usando template gridstack y configuración de recursos
 voila --template=gridstack Proyecto_Bootcamp.ipynb --VoilaConfiguration.resources="{'gridstack': {'show_handles': True}}" --port $PORT --no-browser --Voila.ip=0.0.0.0
-
-chmod +x start.sh
