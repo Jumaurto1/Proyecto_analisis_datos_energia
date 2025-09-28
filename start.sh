84e80b507d74eb8c0d86e85f4786937972da796b
Jumaurto <jumaurto1@gmail.com>
2025-09-28 11:58:17 -0500

    Make start.sh para render

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Levanta el notebook con Voila usando template gridstack y configuración de recursos
-voila --template=gridstack Proyecto_Bootcamp.ipynb \
-      --VoilaConfiguration.resources="{'gridstack': {'show_handles': True}}" \
-      --port $PORT --no-browser --Voila.ip=0.0.0.0
+
+# Definir puerto, por si no está en $PORT
+PORT=${PORT:-8888}
+
+# Ejecutar Voila con template gridstack y theme dark
+exec voila Proyecto_Bootcamp.ipynb \
+     --template=gridstack \
+     --VoilaConfiguration.resources="{'gridstack': {'show_handles': True}}" \
+     --Voila.theme=dark \
+     --port=$PORT \
+     --no-browser \
+     --Voila.ip=0.0.0.0
